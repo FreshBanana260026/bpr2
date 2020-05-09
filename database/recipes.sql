655396c7069e3418c9d363367f4314feb624c6ad
Patrik I <260010@via.dk>
2020-05-09 15:39:41 +0200

    Documentation : sprint reviews udpate

--- database/recipes.sql
+++ database/recipes.sql
@@ -5,5 +5,7 @@ recipename VARCHAR(70) NOT NULL,
 category VARCHAR(25) NOT NULL,
 recipetext TEXT,
 ingredients VARCHAR(100),
+preparation int(11),
+cooking int (11),
 FOREIGN KEY (email) REFERENCES UserLogin(email)
 )
